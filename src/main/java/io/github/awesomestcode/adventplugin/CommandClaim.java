2d1ccee666ec214e3ff88545198d38ed3811a14a
AwesomestAlt <74363858+AwesomestAlt@users.noreply.github.com>
2020-11-13 11:00:44 -0500

    whoops, import player

--- src/main/java/io/github/awesomestcode/adventplugin/CommandClaim.java
+++ src/main/java/io/github/awesomestcode/adventplugin/CommandClaim.java
@@ -5,11 +5,12 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
 
 public class CommandClaim implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(!(sender instanceof player)) {
+        if(!(sender instanceof Player)) {
           sender.sendMessage(ChatColor.RED + "Error: You must be a player to execute this command.");
           return true;
         }
