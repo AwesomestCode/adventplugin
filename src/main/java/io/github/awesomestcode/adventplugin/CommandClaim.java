2186ce1c8543d2164a797bc8bb751df0ed39b937
AwesomestAlt <74363858+AwesomestAlt@users.noreply.github.com>
2020-11-13 09:46:00 -0500

    Create CommandClaim.java

---
+++ src/main/java/io/github/awesomestcode/adventplugin/CommandClaim.java
@@ -0,0 +1,18 @@
+package io.github.awesomestcode.adventplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandClaim implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof player)) {
+          sender.sendMessage(ChatColor.RED + "Error: You must be a player to execute this command.");
+          return true;
+        }
+        sender.sendMessage(ChatColor.AQUA + "This functionality has not yet been implemented.");
+        return true;
+    }
+}
