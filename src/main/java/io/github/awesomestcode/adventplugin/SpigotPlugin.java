680a04e2e313a6b90fefe9cf8ea63ad12c930710
AwesomestAlt <74363858+AwesomestAlt@users.noreply.github.com>
2020-11-13 09:57:08 -0500

    Update SpigotPlugin.java

--- src/main/java/io/github/awesomestcode/adventplugin/SpigotPlugin.java
+++ src/main/java/io/github/awesomestcode/adventplugin/SpigotPlugin.java
@@ -1,11 +1,11 @@
 package io.github.awesomestcode.adventplugin;
 
-import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpigotPlugin extends JavaPlugin {
 
   @Override
-  public void onEnable {
+  public void onEnable() {
     getCommand("claim").setExecutor(new CommandClaim());
   }
 
