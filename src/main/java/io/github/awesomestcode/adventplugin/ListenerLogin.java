b70fcfbd3b199f79c83f71f7704eafa2f59234e1
Raymond Xu <raymond.j.xu@gmail.com>
2020-11-14 12:54:57 -0500

    create login listener

---
+++ src/main/java/io/github/awesomestcode/adventplugin/ListenerLogin.java
@@ -0,0 +1,12 @@
+
+package io.github.awesomestcode.adventplugin;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class ListenerLogin implements Listener {
+    @EventHandler
+    public void onPlayerJoinEvent(PlayerJoinEvent event) {
+    }
+}
