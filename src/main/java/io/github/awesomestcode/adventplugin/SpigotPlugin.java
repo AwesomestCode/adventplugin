41d31e4b5867cf07f4feb3e93d923836d7cf403b
Raymond Xu <raymond.j.xu@gmail.com>
2020-11-14 13:19:12 -0500

    support for file saving

--- src/main/java/io/github/awesomestcode/adventplugin/SpigotPlugin.java
+++ src/main/java/io/github/awesomestcode/adventplugin/SpigotPlugin.java
@@ -1,12 +1,38 @@
 package io.github.awesomestcode.adventplugin;
 
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SpigotPlugin extends JavaPlugin {
+import java.io.File;
+import java.io.IOException;
 
-  @Override
-  public void onEnable() {
-    getCommand("claim").setExecutor(new CommandClaim());
-    getServer().getPluginManager().registerEvents(new ListenerLogin(), this);
-  }
+public class SpigotPlugin extends JavaPlugin {
+    private FileConfiguration customConfig;
+
+    @Override
+    public void onEnable() {
+        getCommand("claim").setExecutor(new CommandClaim());
+        getServer().getPluginManager().registerEvents(new ListenerLogin(), this);
+    }
+    public FileConfiguration getCustomConfig() {
+        return this.customConfig;
+    }
+    private void createCustomConfig() {
+        File customConfigFile = new File(getDataFolder(), "data.yml");
+        if (!customConfigFile.exists()) {
+            //IntelliJ is acting up again.
+            //noinspection ResultOfMethodCallIgnored
+            customConfigFile.getParentFile().mkdirs();
+            saveResource("data.yml", false);
+        }
+
+        customConfig= new YamlConfiguration();
+        try {
+            customConfig.load(customConfigFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
 }
